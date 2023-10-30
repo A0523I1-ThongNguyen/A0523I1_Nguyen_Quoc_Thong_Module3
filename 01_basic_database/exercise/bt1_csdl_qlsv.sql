651f428883f992a379bf70a565dc79eee14df155
A0523I1-ThongNguyen <thongnguyen51323@gmail.com>
2023-10-30 17:40:44 +0700

    Nop bai tap module3.

--- 01_basic_database/exercise/bt1_csdl_qlsv.sql
+++ 01_basic_database/exercise/bt1_csdl_qlsv.sql
@@ -10,3 +10,23 @@ create table teacher(
     age INT ,
     country varchar(50)
 );
+select id,`name` from teacher;
+select * from teacher;
+
+insert into teacher(id,`name`,age)
+values 
+		(1,'NguyenThi',40),
+        (2,'VanLinh',20),
+        (3,'NguyenHuong',35);
+SET SQL_SAFE_UPDATES = 0;-- false , disable primary key
+update teacher
+set id = 5
+where
+	id = 2;
+
+insert into my_class(id,`name`)
+values
+		(1,'TranB'),
+        (2,'NguyenVanTai'),
+        (3,'TruongNgan');
+        
